a31f4f136854a254a71839e0aebbd0da41c5dfd2
Alison Marques <alison.marques@anbetec.com.br>
2022-04-04 22:55:42 -0300

    Criando a estrutura inicial do controller de Contacts

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const port = 3000;
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(routes);
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
