03994fdfec97e8501515d13e45c15ea3a2ba9568
Alison Marques <alison.marques@anbetec.com.br>
2022-04-04 23:57:05 -0300

    Criando as rotas de show, delete e mudando o package uuidv4 para uuid

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,19 +1,42 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Alison',
     email: 'alison@gmail.com',
     phone: '123-456',
-    category: uuid(),
+    category: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Leticia',
+    email: 'leticia@gmail.com',
+    phone: '123-456',
+    category: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(contacts));
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      const oneContact = contacts.find((contact) => contact.id === id);
+      resolve(oneContact);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      // Trazer todos os itens que seja diferente do id passado para deletar
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
