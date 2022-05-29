f5990d3dbdeb3432940236c6bff0e03965bc9be9
Alison Marques <alison.marques@anbetec.com.br>
2022-05-29 00:57:11 -0300

    Capturando e tratando erros

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+// Necessário para error assincronos
+require('express-async-errors');
 const routes = require('./routes');
 
 const port = 3000;
@@ -7,7 +8,12 @@ const port = 3000;
 const app = express();
 // Permitindo que a api receba objetos no body
 app.use(express.json());
-
 app.use(routes);
+
+// Error Handler (Middleware express) -> Manipulador de erros
+app.use((error, request, response, next) => {
+  console.log('errorHandler', error);
+  response.sendStatus(500);
+});
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
