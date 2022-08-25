b1438b6f503b01afdafe85366df3470e469a7cf7
Alison Marques <alison-marques@outlook.com.br>
2022-08-24 22:26:56 -0300

    commit do inicio do projeto até o header do contactslist

---
+++ mycontacts-web/src/components/App/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+/* Usar 100% quando for maior que 500px */
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+`;
