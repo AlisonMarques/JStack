3be19c138f5694ae24fb8e8b790b5dfa97c60437
Alison Marques <alison-marques@outlook.com.br>
2023-01-31 08:47:32 -0300

    add config de cabeçalho no cors

--- mycontacts/src/app/middlewares/cors.js
+++ mycontacts/src/app/middlewares/cors.js
@@ -1,5 +1,9 @@
 module.exports = (request, response, next) => {
   // WildCard -> Curinga * (libera a rota para todos usarem, independente de onde chama.)
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
+
+  response.setHeader('Access-Control-Allow-Methods', '*')
+  response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader('Access-Control-Max-Age', '10')
   next();
 }
