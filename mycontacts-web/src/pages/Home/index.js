aca5bfd7748a44bc9d41bf2da9bd00742db4a871
Alison Marques <alison-marques@outlook.com.br>
2023-03-16 17:50:26 -0300

    exibindo mensagem de nenhum contato encontrado

--- mycontacts-web/src/pages/Home/index.js
+++ mycontacts-web/src/pages/Home/index.js
@@ -4,13 +4,20 @@ import {
 import { Link } from 'react-router-dom';
 
 import {
-  Container, InputSearchContainer, Header, ListHeader, Card, ErrorContainer,
+  Container,
+  InputSearchContainer,
+  Header,
+  ListHeader,
+  Card,
+  ErrorContainer,
+  EmptyListContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
+import emptyBox from '../../assets/images/emptyBox.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -74,17 +81,28 @@ export default function Home() {
       {/* <Modal danger /> */}
       <Loader isLoading={isLoading} />
 
-      <InputSearchContainer>
-        <input
-          type="text"
-          placeholder="Pesquisar contato"
-          value={searchTerm}
-          onChange={handleChangeSearchTerm}
-        />
-      </InputSearchContainer>
+      {contacts.length > 0 && (
+        <InputSearchContainer>
+          <input
+            type="text"
+            placeholder="Pesquisar contato"
+            value={searchTerm}
+            onChange={handleChangeSearchTerm}
+          />
+        </InputSearchContainer>
+      )}
 
-      <Header hasError={hasError}>
-        {!hasError && (
+
+      <Header justifyContent={
+        // eslint-disable-next-line no-nested-ternary
+        hasError ? 'flex-end' : (
+          contacts.length > 0
+            ? 'space-between'
+            : 'center'
+        )
+      }
+      >
+        {(!hasError && contacts.length > 0) && (
           <strong>
             {filteredContacts.length}
             {filteredContacts.length === 1 ? ' contato' : ' contatos'}
@@ -110,6 +128,20 @@ export default function Home() {
 
       {!hasError && (
         <>
+          {(contacts.length < 1 && !isLoading) && (
+            <EmptyListContainer>
+              <img src={emptyBox} alt="Empty Box" />
+
+              <p>
+                Você ainda não possui nenhum contato cadastrado!
+                Clique com o botão
+                <strong> Novo contato </strong>
+                para adicionar um novo contato.
+              </p>
+            </EmptyListContainer>
+          )}
+
+
           {filteredContacts.length > 0 && (
             <ListHeader orderBy={orderBy}>
               <button
