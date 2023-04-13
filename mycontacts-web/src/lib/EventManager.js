0af91cf863177aa7cfb7ecc88c751f12d89e11b2
Alison Marques <alison-marques@outlook.com.br>
2023-04-13 00:13:47 -0300

    Refatorando o EventManager utilizando o Map

--- mycontacts-web/src/lib/EventManager.js
+++ mycontacts-web/src/lib/EventManager.js
@@ -1,38 +1,38 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
     // verifica se existe um evento
-    // caso não exista, cria um array vazio
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    // caso não exista, adicionar um array vazio
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
     // adiciona um listener para um evento
     // Event a chave/nome e listener é a própria função que será executada
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
     // se nao existir evento retorna nada
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
     // percorre todos os listeners e executa a função passando o payload
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeListener(event, listenerToRemove) {
     // desestruturando
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
 
     // se nao existir evento retorna nada
-    if (!this.listeners[event]) {
+    if (!listeners) {
       return;
     }
 
@@ -41,28 +41,6 @@ export default class EventManager {
     const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove);
 
     // atualiza o array de listeners com o novo array filtrado
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
-
-function addToast1(payload) {
-  console.log('Toast adicionado1', payload);
-}
-
-function addToast2(payload) {
-  console.log('Toast adicionado2', payload);
-}
-
-
-const toastEventManager = new EventManager();
-toastEventManager.on('addToast', addToast1);
-toastEventManager.on('addToast', addToast2);
-
-
-toastEventManager.emit('addToast', { type: 'danger', text: 'Texto' });
-
-toastEventManager.removeListener('addToast', addToast1);
-
-toastEventManager.emit('addToast', 'apos');
-
-console.log(toastEventManager);
